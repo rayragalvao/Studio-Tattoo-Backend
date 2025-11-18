b3ab5e21ddf61e3b9f6c22ca2e833f09d3ece627
vivids12 <viviane.santos@sptech.school>
2025-11-18 10:54:26 -0300

    Insert de usuarios para testes

--- Backend/src/main/java/hub/orcana/tables/repository/AgendamentoRepository.java
+++ Backend/src/main/java/hub/orcana/tables/repository/AgendamentoRepository.java
@@ -1,16 +1,15 @@
 package hub.orcana.tables.repository;
 
 import hub.orcana.tables.Agendamento;
+import hub.orcana.tables.StatusAgendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
     List<Agendamento> findByUsuarioId(Long usuarioId);
