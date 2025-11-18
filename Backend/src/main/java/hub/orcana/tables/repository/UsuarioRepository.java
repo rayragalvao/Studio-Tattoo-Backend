ab477dd599f2d4b8b5243c63da02ae3fe4110061
vivids12 <viviane.santos@sptech.school>
2025-11-18 16:48:06 -0300

    Batch de notificação de estoque baixo concluido

--- Backend/src/main/java/hub/orcana/tables/repository/UsuarioRepository.java
+++ Backend/src/main/java/hub/orcana/tables/repository/UsuarioRepository.java
@@ -3,6 +3,7 @@ package hub.orcana.tables.repository;
 import hub.orcana.tables.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
@@ -10,4 +11,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByEmail(String email);
     boolean existsByTelefone(String telefone);
+
+    List<Usuario> findAllByIsAdmin(boolean b);
 }
